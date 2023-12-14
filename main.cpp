f5d8aace072f60499bcad9430fc3394bad8b6ac4
Pizarman13 <dpizagar@myuax.com>
2023-12-14 13:19:20 +0100

    Asignaturas

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 // Created by Pizarman on 14/12/2023.
 //
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 // Creamos la estructura estudiante
 struct Estudiante {
-    char *nombre;
+    std::string nombre;
     int edad;
     float promedio;
+    std::vector<std::string> materias;
 };
 
 // Creamos la clase mostrarEstudiante
@@ -17,9 +20,46 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     std::cout << "Promedio: " << estudiante.promedio << std::endl;
 }
 
+// Agregar Materia
+void agregarMateria(Estudiante& estudiante, const std::string& nuevaMateria) {
+    estudiante.materias.push_back(nuevaMateria);
+}
+
+// Mostrar materias
+void mostrarMaterias(const Estudiante& estudiante) {
+    std::cout << "Materias de " << estudiante.nombre << ":" << std::endl;
+    for (const auto& materia : estudiante.materias) {
+        std::cout << "- " << materia << std::endl;
+    }
+}
+
+// Eliminar materia
+void eliminarMateria(Estudiante& estudiante, const std::string& materiaEliminar) {
+    auto it = std::find(estudiante.materias.begin(), estudiante.materias.end(), materiaEliminar);
+    if (it != estudiante.materias.end()) {
+        estudiante.materias.erase(it);
+        std::cout << "Materia \"" << materiaEliminar << "\" eliminada para " << estudiante.nombre << std::endl;
+    } else {
+        std::cout << "La materia \"" << materiaEliminar << "\" no está en la lista de " << estudiante.nombre << std::endl;
+    }
+}
+
 int main() {
 
-    struct Estudiante est1 = {"Diego", 19, 7.5};
+    struct Estudiante est1 = {"Diego", 19, 7.5, {"Matematicas"}};
+
+    mostrarMaterias(est1);
+
+    // Agregar una materia
+    agregarMateria(est1, "Historia");
+    agregarMateria(est1, "Fisica");
+    std::cout << "\nDespues de agregar Historia y Fisica:\n";
+    mostrarMaterias(est1);
+
+    // Eliminar una materia
+    eliminarMateria(est1, "Fisica");
+    std::cout << "\nDespues de eliminar Física:\n";
+    mostrarMaterias(est1);
 
     mostrarEstudiante(est1);
 
--- mainC.cpp
+++ mainC.cpp
@@ -2,12 +2,18 @@
 // Created by Pizarman on 14/12/2023.
 //
 #include <stdio.h>
+#include <cstring>
+
+#define MAX_MATERIAS 10
+#define MAX_LENGTH_MATERIA 50
 
 // estructura Estudiante
 struct Estudiante {
-    char *nombre;
+    char nombre[20];
     int edad;
     float promedio;
+    char materias[MAX_MATERIAS][MAX_LENGTH_MATERIA];
+    int numMaterias;
 };
 
 // funcion  mostrarEstudinte
@@ -17,11 +23,60 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     printf("Promedio: %.2f\n", estudiante.promedio);
 }
 
+// Función para mostrar las materias en las que está inscrito el estudiante
+void mostrarMaterias(const struct Estudiante* estudiante) {
+    printf("Materias de %s:\n", estudiante->nombre);
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        printf("- %s\n", estudiante->materias[i]);
+    }
+}
+
+// Función para agregar una materia al estudiante
+void agregarMateria(struct Estudiante* estudiante, const char* nuevaMateria) {
+    if (estudiante->numMaterias < MAX_MATERIAS) {
+        strcpy(estudiante->materias[estudiante->numMaterias], nuevaMateria);
+        estudiante->numMaterias++;
+    } else {
+        printf("No se pueden agregar más materias, límite alcanzado.\n");
+    }
+}
+
+// Función para eliminar una materia del estudiante
+void eliminarMateria(struct Estudiante* estudiante, const char* materiaEliminar) {
+    int found = 0;
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        if (strcmp(estudiante->materias[i], materiaEliminar) == 0) {
+            found = 1;
+            for (int j = i; j < estudiante->numMaterias - 1; ++j) {
+                strcpy(estudiante->materias[j], estudiante->materias[j + 1]);
+            }
+            estudiante->numMaterias--;
+            break;
+        }
+    }
+    if (!found) {
+        printf("La materia %s no está en la lista.\n", materiaEliminar);
+    }
+}
+
 int main() {
 
-    struct Estudiante est1 = {"Juan", 20, 5.5};
+    struct Estudiante est1 = {"Juan", 20, 5.5, {"Fisica"}};
 
     mostrarEstudiante(est1);
+
+    // Mostrar materias iniciales
+    mostrarMaterias(&est1);
+
+    // Agregar una materia
+    agregarMateria(&est1, "Historia");
+    printf("\nDespues de agregar Historia:\n");
+    mostrarMaterias(&est1);
+
+    // Eliminar una materia
+    eliminarMateria(&est1, "Mates");
+    printf("\nDespues de eliminar Mates:\n");
+    mostrarMaterias(&est1);
 }
 
 
